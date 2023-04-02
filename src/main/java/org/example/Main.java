86c5ea02c00794810c3cc0275af599fe9d436993
najwansv <najwan.joan.sv@gmail.com>
2023-04-02 14:55:10 +0700

    update UserData

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 
 public class Main {
-
+    // haloo patihhh
     public static void main(String[] args) {
         JFrame next = ChooseLang.main();
         next.setContentPane(new ChooseLang().PanelLanguage);
